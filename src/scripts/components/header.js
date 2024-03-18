855c6deffab9703a21684e95ec6cf3ebf177b944
gerasaka <gerryjulioo@gmail.com>
2024-03-18 07:59:47 +0700

    feat: make header component responsive

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -10,11 +10,28 @@ export class Header extends HTMLElement {
   }
 
   createDrawer() {
+    const btnElement = document.createElement("button");
+    btnElement.innerHTML = `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        height="24px"
+        width="24px"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    `;
+    this.appendChild(btnElement);
+
     this.navWrapper = this.querySelector("nav");
     this.ulElement = this.querySelector("ul");
-
-    this.navWrapper.className = "overlay";
-    this.ulElement.className = "drawer";
 
     const button = this.querySelector("button");
     button.addEventListener("click", () => this.toggleDrawer());
@@ -36,26 +53,8 @@ export class Header extends HTMLElement {
     this.innerHTML = `
       <img src="${eatwellLogo}" alt="logo eatwell" />
 
-      <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          height="24px"
-          width="24px"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
-
-      <nav>
-        <ul>
+      <nav class="overlay">
+        <ul class="drawer">
           <li><a href="index.html">Beranda</a></li>
           <li><a href="#">Favorit</a></li>
           <li><a href="https://github.com/gerasaka">Tentang Kami</a></li>
@@ -63,7 +62,7 @@ export class Header extends HTMLElement {
       </nav>
     `;
 
-    if (window.screen.width < 768) this.createDrawer();
+    this.createDrawer();
   }
 }
 
