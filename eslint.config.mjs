e02847d6ca27397eeb88ec59dc8665bdccbd6f10
gerasaka <gerryjulioo@gmail.com>
2024-04-09 10:13:10 +0700

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { languageOptions: { globals: globals.browser } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
   {
-    ignores: [".config/*", "node_modules/", ".git/", "dist"],
+    ignores: [".config/*", "./node_modules/", ".git/", "./dist"],
   },
   {
     rules: {
