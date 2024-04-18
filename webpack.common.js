ce78d8bec57e62a38e2a0fd211a1a130722ab49f
gerasaka <gerryjulioo@gmail.com>
2024-04-18 11:00:38 +0700

    chore: add workbox

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -31,6 +32,18 @@ module.exports = {
         {
           from: path.resolve(__dirname, "src/public/"),
           to: path.resolve(__dirname, "dist/"),
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: "./sw.bundle.js",
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith("https://restaurant-api.dicoding.dev"),
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "dicoding-restaurant-api",
+          },
         },
       ],
     }),
