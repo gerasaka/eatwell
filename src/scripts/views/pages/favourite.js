c05d9b63d7c6d069c990c30d1426a1c28e0abf84
gerasaka <gerryjulioo@gmail.com>
2024-04-18 17:27:50 +0700

    feat: construct favourite page

--- src/scripts/views/pages/favourite.js
+++ src/scripts/views/pages/favourite.js
@@ -1,10 +1,58 @@
-export default class FavouritePageComponent {
-  async render(pageWrapper) {
-    const el = document.createElement("h1");
-    el.innerHTML = "Favourite";
+import { RestaurantCardComponent } from "../../components";
+import IndexDBService from "../../services/indexDB";
 
-    pageWrapper.appendChild(el);
+export default class FavouritePageComponent {
+  _idbService = undefined;
+
+  constructor() {
+    this._idbService = new IndexDBService();
   }
 
-  async afterRender() {}
+  createFavouriteHeaderEl() {
+    return `
+      <div class="favourite-header">
+        <img src="images/illustration/files.png" alt="gambar ilustrasi file">
+        <h1>Daftar Restoran Favorit</h1>
+      </div>
+    `;
+  }
+
+  createRestaurantListEl() {
+    const wrapperElement = document.createElement("section");
+    wrapperElement.className = "list-wrapper";
+
+    return wrapperElement;
+  }
+
+  async generateRestaurantList() {
+    const restaurantContainer = document.createElement("div");
+    restaurantContainer.className = "restaurant-container";
+
+    const restaurantList = await this._idbService.getAllBookmark();
+
+    if (restaurantList.length) {
+      restaurantList.forEach((restaurant) => {
+        const restaurantItem = new RestaurantCardComponent(restaurant);
+        restaurantContainer.appendChild(restaurantItem);
+      });
+    } else {
+      const emptyWrapper = document.createElement("div");
+      emptyWrapper.className = "empty-wrapper";
+      emptyWrapper.innerHTML = `
+        <img src="../images/illustration/flying-kite.png" alt="ilustrasi list restoran kosong" />
+        <p>Tidak ada restoran favorit, mulai simpan restoran untuk akses yang lebih mudah dan cepat</p>
+      `;
+
+      restaurantContainer.appendChild(emptyWrapper);
+    }
+
+    return restaurantContainer;
+  }
+
+  async render(pageWrapper) {
+    const restaurantListEl = this.createRestaurantListEl();
+    restaurantListEl.appendChild(await this.generateRestaurantList());
+    pageWrapper.innerHTML = this.createFavouriteHeaderEl();
+    pageWrapper.appendChild(restaurantListEl);
+  }
 }
