58bab836fa5fbbd031dddda7fc0ea68f6ee6d44c
gerasaka <gerryjulioo@gmail.com>
2024-04-24 13:08:52 +0700

    test: create test for idb service

---
+++ tests/idb-service.test.js
@@ -0,0 +1,119 @@
+import IndexDBService from "../src/scripts/services/indexDB";
+import { MOCK_KAFE_KITA, MOCK_MELTING_POT } from "./mocks/restaurant-details";
+
+describe("IndexDBService", () => {
+  const _idbService = new IndexDBService();
+
+  describe("putRestaurant function", () => {
+    it("should be able to put new data to DB", async () => {
+      expect(await _idbService.getAllBookmark()).toHaveLength(0);
+
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+      expect(await _idbService.getRestaurant(MOCK_MELTING_POT.id)).toHaveProperty(
+        "id",
+        MOCK_MELTING_POT.id,
+      );
+
+      await _idbService.removevRestaurant(MOCK_MELTING_POT.id);
+    });
+
+    it("should not add duplicate restaurant data", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+      expect(await _idbService.getRestaurant(MOCK_MELTING_POT.id)).toHaveProperty(
+        "id",
+        MOCK_MELTING_POT.id,
+      );
+
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+      expect(await _idbService.getRestaurant(MOCK_MELTING_POT.id)).toHaveProperty(
+        "id",
+        MOCK_MELTING_POT.id,
+      );
+
+      await _idbService.removevRestaurant(MOCK_MELTING_POT.id);
+    });
+
+    it("should not add data when the restaurant param is not given", async () => {
+      expect(await _idbService.putRestaurant()).toEqual({});
+    });
+  });
+
+  describe("getAllBookmark function", () => {
+    it("should return empty array if there is no bookmarked restaurant", async () => {
+      await _idbService.getAllBookmark();
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(0);
+    });
+
+    it("should return all bookmarked restaurant if any", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+
+      await _idbService.putRestaurant(MOCK_KAFE_KITA);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(2);
+
+      await _idbService.removevRestaurant(MOCK_MELTING_POT.id);
+      await _idbService.removevRestaurant(MOCK_KAFE_KITA.id);
+    });
+  });
+
+  describe("removevRestaurant function", () => {
+    it("should be able to remove restaurant with given id", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+      await _idbService.putRestaurant(MOCK_KAFE_KITA);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(2);
+
+      await _idbService.removevRestaurant(MOCK_MELTING_POT.id);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+
+      await _idbService.removevRestaurant(MOCK_KAFE_KITA.id);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(0);
+    });
+
+    it("should not remove restaurant if the id param given is empty or not found", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+
+      await _idbService.removevRestaurant(MOCK_KAFE_KITA.id);
+
+      expect(await _idbService.getAllBookmark()).toHaveLength(1);
+    });
+  });
+
+  describe("getRestaurant function", () => {
+    it("should be able to get restaurant data with given id", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      const restaurantDetails = await _idbService.getRestaurant(MOCK_MELTING_POT.id);
+
+      expect(restaurantDetails).toBeTruthy();
+      expect(restaurantDetails).toHaveProperty("id", MOCK_MELTING_POT.id);
+    });
+
+    it("should not return restaurant data when the data not found", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      const restaurantDetails = await _idbService.getRestaurant(MOCK_KAFE_KITA.id);
+
+      expect(restaurantDetails).toBeFalsy();
+    });
+
+    it("should not return restaurant data when the id param given is empty", async () => {
+      await _idbService.putRestaurant(MOCK_MELTING_POT);
+
+      expect(await _idbService.getRestaurant()).toEqual({});
+    });
+  });
+});
--- tests/mocks/restaurant-details.js
+++ tests/mocks/restaurant-details.js
@@ -41,3 +41,107 @@ export const MOCK_MELTING_POT = {
     },
   ],
 };
+
+export const MOCK_KAFE_KITA = {
+  id: "s1knt6za9kkfw1e867",
+  name: "Kafe Kita",
+  description:
+    "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,",
+  city: "Gorontalo",
+  address: "Jln. Pustakawan no 9",
+  pictureId: "25",
+  categories: [
+    {
+      name: "Sop",
+    },
+    {
+      name: "Modern",
+    },
+  ],
+  menus: {
+    foods: [
+      {
+        name: "Kari kacang dan telur",
+      },
+      {
+        name: "Ikan teri dan roti",
+      },
+      {
+        name: "roket penne",
+      },
+      {
+        name: "Salad lengkeng",
+      },
+      {
+        name: "Tumis leek",
+      },
+      {
+        name: "Salad yuzu",
+      },
+      {
+        name: "Sosis squash dan mint",
+      },
+    ],
+    drinks: [
+      {
+        name: "Jus tomat",
+      },
+      {
+        name: "Minuman soda",
+      },
+      {
+        name: "Jus apel",
+      },
+      {
+        name: "Jus mangga",
+      },
+      {
+        name: "Es krim",
+      },
+      {
+        name: "Kopi espresso",
+      },
+      {
+        name: "Jus alpukat",
+      },
+      {
+        name: "Coklat panas",
+      },
+      {
+        name: "Es kopi",
+      },
+      {
+        name: "Teh manis",
+      },
+      {
+        name: "Sirup",
+      },
+      {
+        name: "Jus jeruk",
+      },
+    ],
+  },
+  rating: 4,
+  customerReviews: [
+    {
+      name: "Ahmad",
+      review: "Tidak ada duanya!",
+      date: "13 November 2019",
+    },
+    {
+      name: "Arif",
+      review: "Tidak rekomendasi untuk pelajar!",
+      date: "13 November 2019",
+    },
+    {
+      name: "Gilang",
+      review: "Tempatnya bagus namun menurut saya masih sedikit mahal.",
+      date: "14 Agustus 2018",
+    },
+    {
+      name: "Mark Thulean",
+      review: "Sedikit mahal",
+      date: "24 April 2024",
+    },
+  ],
+};
