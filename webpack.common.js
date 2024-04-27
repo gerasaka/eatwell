e4394fb2f8e1fdf2b3bb25a2065595621758e19d
gerasaka <gerryjulioo@gmail.com>
2024-04-27 11:01:19 +0700

    chore: add bundle analizer and cplit chunks

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,29 @@ module.exports = {
   externals: {
     sharp: "commonjs sharp",
   },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: "~",
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
